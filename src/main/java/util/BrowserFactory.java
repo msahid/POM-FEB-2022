83279a6db4870548f6b8c14241bcdd7f9e6b360d
Mohammed A Sahid <s4sahid@gmail.com>
2022-10-18 23:48:36 -0400

    pom project

---
+++ src/main/java/util/BrowserFactory.java
@@ -0,0 +1,33 @@
+package util;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserFactory {
+	static WebDriver driver;
+	String url;
+	
+	public  static WebDriver init() {
+
+		System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
+		driver = new ChromeDriver();
+
+//		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().deleteAllCookies();
+		driver.get("https://techfios.com/billing/?ng=login");
+		return driver;
+	}
+	
+	
+	
+	public static void tearDown() {
+		driver.close();
+		driver.quit();
+	}
+
+
+}
